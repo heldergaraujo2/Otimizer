eac12ea2d88cd471215419b5baf6132b0fac1e0e
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-12 20:36:16 -0300

    chore: export routing and optimization APIs

--- importer/src/otimizer_importer/__init__.py
+++ importer/src/otimizer_importer/__init__.py
@@ -1,4 +1,7 @@
+from .metrics import RouteMetrics, calculate_route_metrics
 from .models import Delivery, ImportResult, OptimizedRouteStop, PhysicalStop, Route
+from .optimizer import OptimizationError, optimize_nearest_neighbor
+from .routing import RoutingError, TravelMetric, build_osrm_table_url, fetch_osrm_table, parse_osrm_table
 from .xlsx import import_deliveries, import_result
 
 __all__ = [
@@ -7,6 +10,15 @@ __all__ = [
     "OptimizedRouteStop",
     "PhysicalStop",
     "Route",
+    "RouteMetrics",
+    "TravelMetric",
+    "OptimizationError",
+    "RoutingError",
+    "build_osrm_table_url",
+    "calculate_route_metrics",
+    "fetch_osrm_table",
     "import_deliveries",
     "import_result",
+    "optimize_nearest_neighbor",
+    "parse_osrm_table",
 ]
