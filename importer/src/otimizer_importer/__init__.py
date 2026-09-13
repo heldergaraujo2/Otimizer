f8e4d8ac4222c5a139367bb1619ef51c9bc9fb43
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-12 21:28:15 -0300

    feat: export optimization service and routing providers

--- importer/src/otimizer_importer/__init__.py
+++ importer/src/otimizer_importer/__init__.py
@@ -1,28 +1,24 @@
 from .models import Delivery, ImportResult, OptimizedRouteStop, PhysicalStop, Route
 from .optimization import OptimizationObjective, OptimizationProblem, OptimizationResult, RouteEndpoint, optimize
 from .optimizer import OptimizationError, optimize_nearest_neighbor
-from .routing import RoutingError, TravelMetric, build_osrm_table_url, build_route_matrix, fetch_osrm_table, parse_osrm_table
+from .routing import (
+    OSRMRoutingProvider,
+    RoutingError,
+    RoutingProvider,
+    TravelMetric,
+    build_osrm_table_url,
+    build_route_matrix,
+    fetch_osrm_table,
+    parse_osrm_table,
+)
+from .service import OptimizationServiceResult, optimize_deliveries_file
 from .xlsx import import_deliveries, import_result
 
 __all__ = [
-    "Delivery",
-    "ImportResult",
-    "OptimizedRouteStop",
-    "PhysicalStop",
-    "Route",
-    "OptimizationError",
-    "OptimizationObjective",
-    "OptimizationProblem",
-    "OptimizationResult",
-    "RouteEndpoint",
-    "RoutingError",
-    "TravelMetric",
-    "build_osrm_table_url",
-    "build_route_matrix",
-    "fetch_osrm_table",
-    "import_deliveries",
-    "import_result",
-    "optimize",
-    "optimize_nearest_neighbor",
-    "parse_osrm_table",
+    "Delivery", "ImportResult", "OptimizedRouteStop", "PhysicalStop", "Route",
+    "OptimizationError", "OptimizationObjective", "OptimizationProblem", "OptimizationResult", "RouteEndpoint",
+    "RoutingError", "RoutingProvider", "OSRMRoutingProvider", "TravelMetric",
+    "build_osrm_table_url", "build_route_matrix", "fetch_osrm_table", "parse_osrm_table",
+    "OptimizationServiceResult", "optimize_deliveries_file",
+    "import_deliveries", "import_result", "optimize", "optimize_nearest_neighbor",
 ]
