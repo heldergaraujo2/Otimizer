0f4be8595f4b9af21d7f41ab5bed623250eac80e
heldergaraujo2 <heldergaraujo2@gmail.com>
2026-09-12 21:45:16 -0300

    feat: export route result metrics API

--- importer/src/otimizer_importer/__init__.py
+++ importer/src/otimizer_importer/__init__.py
@@ -1,3 +1,4 @@
+from .metrics import RouteLeg, RouteMetrics, calculate_route_metrics
 from .models import Delivery, ImportResult, OptimizedRouteStop, PhysicalStop, Route
 from .optimization import OptimizationObjective, OptimizationProblem, OptimizationResult, RouteEndpoint, optimize
 from .optimizer import OptimizationError, optimize_nearest_neighbor
@@ -19,6 +20,7 @@ __all__ = [
     "OptimizationError", "OptimizationObjective", "OptimizationProblem", "OptimizationResult", "RouteEndpoint",
     "RoutingError", "RoutingProvider", "OSRMRoutingProvider", "TravelMetric",
     "build_osrm_table_url", "build_route_matrix", "fetch_osrm_table", "parse_osrm_table",
+    "RouteLeg", "RouteMetrics", "calculate_route_metrics",
     "OptimizationServiceResult", "optimize_deliveries_file",
     "import_deliveries", "import_result", "optimize", "optimize_nearest_neighbor",
 ]
